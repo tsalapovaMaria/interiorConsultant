7e9923dfc951a8298cca8fa15734606254d6d56b
Maria <mazhats@yandex.ru>
2021-06-11 22:08:37 +0300

    add clicked nav-items style

--- burger-menu.js
+++ burger-menu.js
@@ -1,5 +1,15 @@
 const button = document.querySelector('.burger-menu');
+const navigation = document.querySelector('.navigation');
+
+const navItems = document.querySelectorAll('.nav-item');
 
 button.onclick = () => {
     button.classList.toggle('opened-menu');
-}
+    navigation.hidden = !navigation.hidden;
+};
+
+Array.from(navItems).forEach(navItem => {
+    navItem.onclick = () => {
+        navItem.classList.toggle('clicked-nav-item');
+    }
+});
